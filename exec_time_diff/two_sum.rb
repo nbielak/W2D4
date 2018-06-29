494c98286b6819f8aafab696be18b09fb0c3c639
Nick Battjes <nhbattjes@gmail.com>
2018-06-28 17:08:48 -0700

    Work on okay_two_sum

--- exec_time_diff/two_sum.rb
+++ exec_time_diff/two_sum.rb
@@ -1,3 +1,4 @@
+# n^2
 def bad_two_sum?(array, target_value)
   array.each_with_index do |el1, idx1|
     array.each_with_index do |el2, idx2|
@@ -6,6 +7,24 @@ def bad_two_sum?(array, target_value)
     end 
   end
   false
+end
+
+def okay_two_sum?(array, target_value)
+  sorted_arr = array.sort
+  
+  
+end
+
+def okay_two_sum?(array, target_value)
+  max = array.shift
+  so_far = max
+  
+  array.each do |el|
+    max = (target_value - el > target_value - max + el ) ? el : max + el
+    so_far = (so_far > max) ? so_far : max
+  end
+  
+  so_far
 end
 
 arr = [0, 1, 5, 7]
